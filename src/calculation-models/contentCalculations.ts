58209904f4812c8f3ca3101018d3ab36c0990b15
mohsen <mohsen.bahrami7690@gmail.com>
2023-02-21 19:38:39 +0330

    set variable to HTML tags for Interaction

--- src/calculation-models/contentCalculations.ts
+++ src/calculation-models/contentCalculations.ts
@@ -1,11 +1,11 @@
 // ++ Content Calculations
 const DOMElement: HTMLElement = document.documentElement;
 
-export const LinkArray: RegExpMatchArray | null = DOMElement.innerHTML.match(/\b(href|src)\=[\'\"](http|ftp|smtp|pop|udp|ip|tcp).*?[\'\"]/g);
+export const linkArray: RegExpMatchArray | null = DOMElement.innerHTML.match(/\b(href|src)\=[\'\"](http|ftp|smtp|pop|udp|ip|tcp).*?[\'\"]/g);
 
 export const videoTagArry: HTMLCollectionOf<HTMLVideoElement> = DOMElement.getElementsByTagName("video");
 
-export const ausioTagArry: HTMLCollectionOf<HTMLAudioElement> = DOMElement.getElementsByTagName("audio");
+export const audioTagArry: HTMLCollectionOf<HTMLAudioElement> = DOMElement.getElementsByTagName("audio");
 
 export const imgTagArry: HTMLCollectionOf<HTMLImageElement> = DOMElement.getElementsByTagName("img");
 
